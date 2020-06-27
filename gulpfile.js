22da8859ae6047d158f29b97fe45aa849fa45ab5
Ivan Wilson <ivanwilson70@gmail.com>
2020-06-26 23:39:10 -0400

    Quick Gulp functionality (CSS/SASS)

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,25 @@
-const { src, dest } = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create(); //compile scss into css
 
-gulp.task('sass', function() {
+function style() {
     return gulp.src('app/build/css/widget.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-gulp.task('watch', ['browserSync', 'sass'], function() {
-    gulp.watch('app/build/css/*.scss', ['sass']);
-    // Other watchers
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/*.js', browserSync.reload);
-});
-
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
-    })
-});
-
-
-
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+        .pipe(browserSync.stream());
 }
 
-exports.default = defaultTask
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./app",
+            index: "/index.html"
+        }
+    });
+    gulp.watch('./build/css/widget.scss', style)
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    //gulp.watch('./js/*.js').on('change', browserSync.reload);
+}
+
+exports.watch = watch;
+exports.style = style;
