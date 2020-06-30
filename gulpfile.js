5c3ac39458f7ec7b338870e8f73e97570a975945
Ivan Wilson <ivanwilson70@gmail.com>
2020-06-29 23:41:04 -0400

    Updating gulpfile for CSS conversions

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const pxtorem = require('postcss-pxtorem');
 const browserSync = require('browser-sync').create(); //compile scss into css
+const plumber = require('gulp-plumber');
+const cssnano = require("cssnano");
+
+const processors = [
+    pxtorem({
+        rootValue: 10,
+        unitPrecision: 5,
+        propList: ['font*', 'line-height', 'letter-spacing', 'word-spacing', 'margin*', 'padding*'],
+        replace: true
+    }),
+    autoprefixer()
+];
 
 function style() {
     return gulp.src('app/build/css/widget.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss(processors))
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream());
 }
@@ -21,6 +38,19 @@ function watch() {
     //gulp.watch('./js/*.js').on('change', browserSync.reload);
 }
 
+function buildcss() {
+    return gulp.src('app/css/widget.css')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss(cssnano()))
+        .pipe(gulp.dest('app/css'))
+}
+
+function buildjs() {
+
+}
+
 exports.watch = watch;
 exports.style = style;
 exports.default = gulp.series(style, watch);
+exports.build = gulp.parallel(buildcss, buildjs);
